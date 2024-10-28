7ce12aad6ac04140d3be9105d02917dcce97a1e6
swapnil <swapnil130501@gmail.com>
2024-10-28 13:33:20 +0530

    update todo reducer

--- src/reducer/todoReducer.js
+++ src/reducer/todoReducer.js
@@ -1,17 +1,42 @@
+// function todoReducer(state, action) {
+//     console.log(state, action)
+//     switch (action.type) {
+//         case 'ADD_TODO':
+//             return [...state, { value: action.payload }];
+//         case 'DELETE_TODO':
+//             return state.filter(todo => todo.value !== action.payload);
+//         case 'UPDATE_TODO':
+//             return state.map(todo => 
+//                 todo.value === action.payload.oldValue 
+//                 ? { value: action.payload.newValue } 
+//                 : todo
+//             );
+//         default:
+//             return state;
+//     }
+// }
+
+// export default todoReducer;
+
 function todoReducer(state, action) {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [...state, { value: action.payload }];
-        case 'DELETE_TODO':
-            return state.filter(todo => todo.value !== action.payload);
-        case 'UPDATE_TODO':
-            return state.map(todo => 
-                todo.value === action.payload.oldValue 
+    if (action.type === 'ADD_TODO') {
+        return [...state, { value: action.payload }];
+    } 
+
+    else if (action.type === 'DELETE_TODO') {
+        return state.filter((todo) => todo.value !== action.payload);
+    } 
+
+    else if (action.type === 'UPDATE_TODO') {
+        return state.map((todo) =>
+            todo.value === action.payload.oldValue 
                 ? { value: action.payload.newValue } 
                 : todo
-            );
-        default:
-            return state;
+        );
+    } 
+    
+    else {
+        return state; 
     }
 }
 
